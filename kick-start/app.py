812ed9f6e3c815018b53453d5b7734dc8639a15c
unknown <xuberswork@gmail.com>
2024-12-26 01:14:07 +0530

    Improvements in app.py - #DataModel

--- kick-start/app.py
+++ kick-start/app.py
@@ -1,4 +1,6 @@
+import uuid
 from flask import Flask , request
+from db import items,stores
 
 app = Flask(__name__)
 
@@ -9,46 +11,52 @@ then data from the list will be lost
 because it is not persisted unlike the database
 
 """
+@app.get('/get_stores')
+def get_all_stores():
+    if stores:
+        return stores
+    return {'message': 'No Stores Registered'},404
 
-stores = [
-    {
-        'name': 'My Store',
-        'items': [
-            {
-                'name': 'Chair',
-                'price': 15.99
-            }
-        ]
-    }
-]
+@app.get('/get_items')
+def get_all_items():
+    if items:
+        return items
+    return {'message': 'No Items Registered'},404
 
-@app.get('/store')
-def get_store():
-    return {'stores': stores}
+@app.get('/store/<string:store_id>')
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {'message': 'Store not found'},404
+    
+@app.get('/item/<string:item_id>')
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {'message': 'Item not found'},404
 
 @app.post('/store')
 def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    stores.append(new_store)
-    return new_store,201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, 'id': store_id}
+    stores[store_id] = store
+    return store,201
 
-@app.post('/store/<string:name>/item')
+@app.post('/item')
 def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {
-                "name":request_data['name'],
-                "price":request_data['price']
-            }
-            store['items'].append(new_item)
-            return new_item,201
-    return {'message': 'Store not found'},404
-
+    item_data = request.get_json()
+    item_id = uuid.uuid4().hex
+    if item_data["store_id"] not in stores:
+        return {'message': 'Store not found'},404
+    item = {**item_data, 'id': item_id}
+    items[item_id] = item
+    return item,201
+    
+"""   
+/*
 @app.get('/store/<string:name>')
 def get_store_by_name(name):
     for store in stores:
@@ -62,3 +70,7 @@ def get_store_items(name):
         if store['name']==name:
             return store['items']
     return {'message': 'Store not found'},404
+
+*/
+
+"""
